5593549b7e15e08e78e60eda03f2e944c312079e
Иван Иванов <alatarev@yandex.ru>
2019-11-24 18:15:24 +0300

    Добавлено редактирование(с невыявленными ошибками)

--- kursov/kursov.cpp
+++ kursov/kursov.cpp
@@ -8,6 +8,20 @@
 
 FILE *file;
 
+void put(char str[], int n)
+{
+	for (int i = 0; str[i+n] != '|'; i++)
+		str[i+n] = ' ';
+}
+
+void zero(char str[])
+{
+	for (int i = 0; i < sizeof(str); i++)
+	{
+		str[i] = '\0';
+	}
+}
+
 void replenishment()
 { 
 	file = fopen("base.txt", "r+");
@@ -47,7 +61,7 @@ void replenishment()
 	while (*ptr != '\0')
 	{
 		*(l + i) = *ptr;
-		*(l++)+i;
+		*l++;
 		*ptr++;
 	}
 
@@ -61,11 +75,12 @@ void replenishment()
 			else
 			printf("Неверный формат\n");
 	} while (flag==0);
+	flag = 0;
 	i = 33;
 	while (*ptr != '\0')
 	{
 		*(l + i) = *ptr;
-		*(l++) + i;
+		*l++;
 		*ptr++;
 	}
 
@@ -79,11 +94,12 @@ void replenishment()
 		else
 			printf("Неверный формат\n");
 	} while (flag == 0);
+	flag = 0;
 	i = 44;
 	while (*ptr != '\0')
 	{
 		*(l + i) = *ptr;
-		*(l++) + i;
+		*l++;
 		*ptr++;
 	}
 
@@ -95,7 +111,7 @@ void replenishment()
 	while (*ptr != '\0')
 	{
 		*(l + i) = *ptr;
-		*(l++) + i;
+		*l++;
 		*ptr++;
 	}
 	*(l + i + 1) = ' ';
@@ -119,26 +135,143 @@ void editing()
 	char time_d[6] = { '\0' };
 	char time_a[6] = { '\0' };
 	char price[11] = { '\0' };
-	char *ptr;
 	char *l;
-	char flight[3] = {'\0'};
+	char *ptr;
+	int flag = 0;
+	int flight = 0;
+	int n = 0;
+	int number = 0;
 
 	system("CLS");
 
+	l = fgets(line, MAXLINE, file);
+
 	printf("- Редактирование базы данных -\n");
-	printf("Введите номер рейса для редактирования: ");
-	scanf("%s", &flight);
+	do {
+		printf("Введите номер рейса для редактирования: ");
+		scanf("%d", &flight);
 
-	l = fgets(line, MAXLINE, file);
-	l = fgets(line, MAXLINE, file);
+		for (int i = 0; i < flight; i++)
+		{
+			l = fgets(line, MAXLINE, file);
+		}
+		if (l == NULL)
+		{
+			printf("Error!");
+		}
 
-	for (int i = 0; i < 3; i++)
+		if (*(l + 4) != ' ')
+			flag = 1;
+		else
+		{
+			printf("Рейса с таким номером не существует\n");
+			flag = 0;
+			rewind(file);
+			l = fgets(line, MAXLINE, file);
+		}
+	} while (flag == 0);
+	flag = 0;
+
+	system("CLS");
+	printf("Выберете что требуется изменить: \n");
+	printf("1 - Пункт прилета(Destination) \n");
+	printf("2 - Время вылета() \n");
+	printf("3 - Время прилета() \n");
+	printf("4 - Стоимость() \n\n");
+	scanf("\n%d", &number);
+
+	if ((number < 1) && (number > 4))
+		printf("Ошибка. Выбрано несуществующее действие");
+
+	switch (number)
 	{
+	case 1:
+		system("CLS");
 
+		n = 4;
+		put(line, n);
+		printf("Введите новый пункт прилета: ");
+		scanf("%s", &destination);
+
+		for (int i = 0; destination[i] != '\0'; i++)
+		{
+			*(l + n+i) = destination[i];
+		}
+
+		break;
+
+	case 2:
+		system("CLS");
+
+		n = 38;
+		put(line, n);
+		do
+		{
+			printf("Введите новое время вылета в фромате '00:00': ");
+			scanf("%s", &time_d);
+			ptr = time_d;
+			if ((*ptr >= '0' && *ptr <= '9') && (*(ptr + 1) >= '0' && *(ptr + 1) <= '9') && (*(ptr + 3) >= '0' && *(ptr + 3) <= '9') && (*(ptr + 4) >= '0' && *(ptr + 4) <= '9') && (*(ptr + 2) == ':'))
+				flag = 1;
+			else
+				printf("Неверный формат\n");
+		} while (flag == 0);
+		flag = 0;
+
+		for (int i = 0; time_d[i] != '\0'; i++)
+		{
+			*(l + n + i) = time_d[i];
+		}
+
+		break;
+	case 3:
+		system("CLS");
+		n = 54;
+		put(line, n);
+		do
+		{
+			printf("Введите новое время прилета в фромате '00:00': ");
+			scanf("%s", &time_a);
+			ptr = time_a;
+			if ((*ptr >= '0' && *ptr <= '9') && (*(ptr + 1) >= '0' && *(ptr + 1) <= '9') && (*(ptr + 3) >= '0' && *(ptr + 3) <= '9') && (*(ptr + 4) >= '0' && *(ptr + 4) <= '9') && (*(ptr + 2) == ':'))
+				flag = 1;
+			else
+				printf("Неверный формат\n");
+		} while (flag == 0);
+		flag = 0;
+
+		for (int i = 0; time_a[i] != '\0'; i++)
+		{
+			*(l + n + i) = time_a[i];
+		}
+		break;
+	case 4:
+		system("CLS");
+		n = 70;
+		put(line, n);
+		printf("Введите новую стоимость: ");
+		scanf("%s", &price);
+		for (int i = 0; price[i] != '\0'; i++)
+		{
+			*(l + n + i) = price[i];
+		}
+		*(l + n + 1) = ' ';
+		*(l + n + 2) = 'r';
+		*(l + n + 3) = 'u';
+		*(l + n + 4) = 'b';
+		break;
+
+		fseek(file, 88 * flight, SEEK_SET);
+		fputs(blank, file);
+		fseek(file, 88 * flight, SEEK_SET);
+		fputs(line, file);
+		fclose(file);
 	}
+	fseek(file, 88 * flight, SEEK_SET);
+	fputs(blank, file);
+	fseek(file, 88 * flight, SEEK_SET);
+	fputs(line, file);
 
-
-
+	fclose(file);
 }
 
 void deleting()
