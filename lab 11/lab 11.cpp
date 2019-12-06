04cc1da36d83fbca4d51068f13ab885a9a4f38ca
Иван Иванов <alatarev@yandex.ru>
2019-12-06 16:21:39 +0300

    комментарии

--- lab 11/lab 11.cpp
+++ lab 11/lab 11.cpp
@@ -17,21 +17,22 @@ int main(int argc, char* argv[])
 	HANDLE hConsoleHandle = GetStdHandle(STD_OUTPUT_HANDLE);
 
 	char filename[100] = { 0 }; // путь до файла
-	char line[1024];
-	char string[184];
-	int pos_w[30];
-	char word[30];
-	int cnt = 0;
+	char line[1024];// строка для обработки
+	char string[184];//строка содержащая данные из файла "slova"
+	int pos_w[30]; //позиции первых символов в строке string
+	char word[30]; //строка для храния слов
+	int cnt = 0; 
 	int n = 0;
 	char *ptr;
 	char *p=string;
 	int flag = 0;
-	int begin = 1;
-	int end = 1;
+	int begin = 1;//позиция начала слова
+	int end = 1;//позиция конца слова
 	char *str=word;
+	int z = 0;//запоминатель для конца слова
 	int found = 0;
 
-/*
+
 	if (argc == 2)
 		strcpy(filename, argv[1]);
 	else
@@ -46,100 +47,97 @@ int main(int argc, char* argv[])
 				printf("Cannot open input file.\n");
 			}
 		} while (fpin == NULL);
-	}*/
+	}
 
-	fpin = fopen("test.txt", "r");// результирующий файл
-	if (fpin == NULL)
-		return 0;
 
-	sl = fopen("slova.txt", "r");
+	sl = fopen("slova.txt", "r");//открытия фала где хранятся ключевые слова
 	if (sl == NULL)
 		return 0;
 
 
 	while (!feof(sl))
 	{
-		*p=fgetc(sl);
+		*p=fgetc(sl);//посимвольное считывание
 		if (*p == ' ')
 		{
-			*p = '\0';
+			*p = '\0';//пробелы заменяются на символ конца строки
 			flag = 1;
 		}
-		else if (flag != 0)
+		else if (flag != 0)//если первый символ после пробела
 		{
-			pos_w[n++]= cnt;
+			pos_w[n++]= cnt;// запоминается позиция этого слова
 			flag = 0;
 		}
 		cnt++;
 		*p++;
 	}
+
 	n = 0;
 	cnt = 0;
-	int z = 0;
 	flag = 0;
+
 	while (!feof(fpin))
 	{
-		ptr = fgets(line, 1024, fpin);
-
-		if (ptr == NULL)
-			break;
-		z = 0;
-		while (cnt != 3)
-		{
-			for (int i = z; *(ptr + i) != '\0'; i++)
-			{
-				zero(word);
-				for (n = 0; (*(ptr + i) >= 'a' && *(ptr + i) <= 'z'); n++)
-				{
-					if (flag == 0)
-					{
-						flag = 1;
-						begin = i;
-					}
-					*(str + n) = *(ptr + i);
-					i++;
-					end = i;
-				}
-				if (flag == 1)
-				{
-					flag = 0;
-					for (int j = 0; j < 29; j++)
-					{
-						if (strcmp(word, &string[pos_w[j]]) == 0)
-						{
-							found = 1;
-							break;
-						}
-					}
-				}
-				if (found == 1)
-					break;
-			}
-			SetConsoleTextAttribute(hConsoleHandle, 15);
-			for (int i = z; i<end; i++)
-			{
-				if (found == 1)
-				{
-					if (i == begin)
-						SetConsoleTextAttribute(hConsoleHandle, 10);
-				}
-				printf("%c", *(ptr + i));
-			}
-			z = end;
-			cnt++;
-			found = 0;
-			begin = 1;
-			flag = 0;
-		}
-		SetConsoleTextAttribute(hConsoleHandle, 15);
-		for(int i = end; *(ptr+i)!='\0';i++)
-			printf("%c", *(ptr + i));
-
 		found = 0;
 		begin = 1;
 		end = 1;
 		flag = 0;
 		cnt = 0;
-	
+		z = 0;
+
+		ptr = fgets(line, 1024, fpin);//считывание строки из файла
+
+		if (ptr == NULL)
+			break;
+
+		while (cnt != 3)
+		{
+			for (int i = z; *(ptr + i) != '\0'; i++)//рассматириваем строку
+			{
+				zero(word);//обнуление массива со словами
+				for (n = 0; (*(ptr + i) >= 'a' && *(ptr + i) <= 'z'); n++)// если символы буквы, то они записываются как слово
+				{
+					if (flag == 0)
+					{
+						flag = 1;
+						begin = i;//запоминание начала слова
+					}
+					*(str + n) = *(ptr + i);
+					i++;
+					end = i;//запоминание конца слова
+				}
+				if (flag == 1)//если есть слово
+				{
+					flag = 0;
+					for (int j = 0; j < 29; j++)
+					{
+						if (strcmp(word, &string[pos_w[j]]) == 0)//сравниваем слово на совпадение с ключевыми словами
+						{
+							found = 1;//признак совпадения
+							break;
+						}
+					}
+				}
+				if (found == 1)// если есть совпадение, выход из цикла
+					break;
+			}
+			SetConsoleTextAttribute(hConsoleHandle, 15);//белый цвет шрифта
+			for (int i = z; i<end; i++)//от позиции начала строки или предыдущего слова до конца текущего слова
+			{
+				if (found == 1)//если было совпадение
+				{
+					if (i == begin)//при начале слова
+						SetConsoleTextAttribute(hConsoleHandle, 10);//зеленый цвет
+				}
+				printf("%c", *(ptr + i));//вывод символов на экран
+			}
+			z = end;//запоминание конца предыдущего слова 
+			cnt++;
+			found = 0;
+			flag = 0;
+		}
+		SetConsoleTextAttribute(hConsoleHandle, 15);
+		for(int i = end; *(ptr+i)!='\0';i++)//вывод остатка слова
+			printf("%c", *(ptr + i));	
 	}
 }
